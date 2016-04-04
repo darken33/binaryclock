c865a00dbe9cc59e2042aa5be644e69c0d9bed4d
darken33 <darken33@free.fr>
2016-04-04 06:29:21 +0200

    v1.1 - 20120215 - Major features : - Use of phoneGap Framework - Vibrate the phone when changing page - Detect pressing the back button to close the application

--- mobile/gen/fr/free/darken33/binaryclock/R.java
+++ mobile/gen/fr/free/darken33/binaryclock/R.java
@@ -12,9 +12,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int webview=0x7f050000;
+        public static final int appView=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- mobile/src/fr/free/darken33/binaryclock/BinaryClockActivity.java
+++ mobile/src/fr/free/darken33/binaryclock/BinaryClockActivity.java
@@ -1,38 +1,21 @@
 package fr.free.darken33.binaryclock;
 
-import fr.free.darken33.binaryclock.R;
-import android.app.Activity;
+import com.phonegap.*;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.webkit.WebView;
 
-public class BinaryClockActivity extends Activity {
-	/** La vue Web */
-	WebView mWebView;
-	
-	/** 
-	 * Méthode appelé lors de la création de l'application. 
-	 */
+/**
+ * Activité de Montre Binaire
+ * 
+ * @author philippe bousquet <darken33@free.fr>
+ * @version 1.1
+ * @license Gnu GPL v3
+ */
+public class BinaryClockActivity extends DroidGap {
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new BinaryClockWebClient());        
-        mWebView.loadUrl("file:///android_asset/www/index.html");        
+        super.loadUrl("file:///android_asset/www/index.html");
     }
-    
-    /**
-     * Evenement sur la touche retour
-     */
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
-        	this.finish();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }   
 }
